512017b377bdc6d4ed23a887d4d158590fd8689a
PHOYSAL <phoysal0007@gmail.com>
2025-03-01 11:51:21 +0600

    add money and cash out btn active

---
+++ js/features.js
@@ -0,0 +1,8 @@
+document.getElementById('add-money-feature-btn')
+.addEventListener('click',function(){
+    showFeature('add-money-form')
+});
+document.getElementById('cash-out-feature-btn')
+.addEventListener('click',function(){
+    showFeature('cash-out-form')
+})
--- js/utilities.js
+++ js/utilities.js
@@ -7,3 +7,10 @@ function getTextFieldValue(id){
     const balanceText=Number(document.getElementById(id).innerText);
     return balanceText
 }
+
+function showFeature(id){
+    document.getElementById("add-money-form").classList.add('hidden')
+    document.getElementById("cash-out-form").classList.add('hidden')
+
+    document.getElementById(id).classList.remove('hidden')
+}
